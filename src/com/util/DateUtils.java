bcdc852755dfdc21e6fbc8d7e5ea5d78d9d527e6
vineethsnair <vineethsnair4u@gmail.com>
2018-12-22 00:48:14 +0530

    Initial commit for Ant Demo Project

---
+++ src/com/util/DateUtils.java
@@ -0,0 +1,17 @@
+package com.util;
+
+import java.util.Date;
+
+public class DateUtils {
+
+	public static void main(String[] args) {
+
+		System.out.println(getLocalCurrentDate());
+		
+	}
+
+	private static Date getLocalCurrentDate() {
+		return new Date();		
+	}
+
+}
---
+++ src/com/util/PrintUitl.java
@@ -0,0 +1,7 @@
+package com.util;
+
+public class PrintUitl {
+/*public static void main(String args []) {
+	System.out.println("This is an Ant Build demo");
+}*/
+}
